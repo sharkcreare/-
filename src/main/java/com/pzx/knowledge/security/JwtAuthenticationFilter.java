e8d4e631d77a323a93af2392780fd97fbe1ffc24
pan <3416515278@qq.com>
2026-07-29 10:41:13 +0800

    登录鉴权功能

---
+++ src/main/java/com/pzx/knowledge/security/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.pzx.knowledge.security;
+
+import com.pzx.knowledge.utils.JwtUtils;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+            String token = getTokenFromRequest(request);
+
+            if (StringUtils.hasText(token)&&jwtUtils.validateToken(token)){
+                Long userId =jwtUtils.getUserId(token);
+                UsernamePasswordAuthenticationToken authentication=
+                        new UsernamePasswordAuthenticationToken(userId, null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        chain.doFilter(request, response);
+    }
+
+
+    private String getTokenFromRequest(HttpServletRequest request){
+        String bearer= request.getHeader("Authorization");
+        if (StringUtils.hasText(bearer)&&bearer.startsWith("Bearer ")){
+            return bearer.substring(7);
+        }
+        return null;
+    }
+}
